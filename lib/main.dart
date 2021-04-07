ec3ef9b9ee47a7fe64291f39e55346a7c3aae6ab
Doug Bell <doug101@bellz.org>
2021-04-06 21:33:22 -0400

    added file chooser

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,72 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'src/tree_core.dart';
 
-void main() => runApp(TreeMobileApp());
+void main() {
+  runApp(MaterialApp(
+    title: 'TreeLine Mobile',
+    home: FileControl(),
+  ));
+}
 
-class TreeMobileApp extends StatelessWidget {
+class FileControl extends StatefulWidget {
+  @override
+  State<FileControl> createState() => _FileControlState();
+}
+
+class _FileControlState extends State<FileControl> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TreeLine Mobile',
-      home: TreeMobile(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TreeLine Mobile'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text('File Browse'),
+              onTap: () async {
+                FilePickerResult? result = await FilePicker.platform.pickFiles();
+                if (result != null) {
+                  PlatformFile fileObj = result.files.single;
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TreeView(fileObj: fileObj),
+                    ),
+                  );
+                  //FilePicker.platform.clearTemporaryFiles();
+                };
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-class _TreeMobileState extends State<TreeMobile> {
-  static const _fn = '/sdcard/Download/SFBooks.trln';
+class TreeView extends StatefulWidget {
+  final PlatformFile fileObj;
+
+  TreeView({Key? key, required this.fileObj}) : super(key: key);
+
+  @override
+  State<TreeView> createState() => _TreeViewState();
+}
+
+class _TreeViewState extends State<TreeView> {
   final _closedIcon = Icon(Icons.arrow_right, size: 24.0, color: Colors.blue);
   final _openIcon = Icon(Icons.arrow_drop_down, size: 24.0, color: Colors.blue);
   final _leafIcon = Icon(Icons.circle, size: 8.0, color: Colors.blue);
-  final _treeStructure = TreeStructure(_fn);
+  late final _treeStructure;
   final _openSpots = <TreeSpot>{};
+  void initState() {
+    super.initState();
+    _treeStructure = TreeStructure(widget.fileObj.path!);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,9 +119,4 @@ class _TreeMobileState extends State<TreeMobile> {
       ),
     );
   }
-}
-
-class TreeMobile extends StatefulWidget {
-  @override
-  State<TreeMobile> createState() => _TreeMobileState();
 }
