d0e139457d0035538aa332593f966df52413193e
Doug Bell <doug101@bellz.org>
2021-06-29 21:19:30 -0400

    restructure code for fields into a separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'src/tree_core.dart';
+import 'src/tree_struct.dart';
 
 void main() {
   runApp(MaterialApp(
---
+++ lib/src/tree_fields.dart
@@ -0,0 +1,132 @@
+import 'dart:convert' show HtmlEscape;
+import 'package:intl/intl.dart' show DateFormat;
+import 'tree_struct.dart' show TreeNode;
+
+
+/// A portion of the data held within a node.
+class Field {
+  late String name, _type, _format, _prefix, _suffix;
+  var boolFormat = {true: 'yes', false: 'no'};
+
+  Field(Map<String, dynamic> jsonData) {
+    name = jsonData['fieldname'] ?? '';
+    _type = jsonData['fieldtype'] ?? 'Text';
+    _format = jsonData['format'] ?? '';
+    _prefix = jsonData['prefix'] ?? '';
+    _suffix = jsonData['suffix'] ?? '';
+    if ({'Date', 'Time', 'DateTime'}.contains(_type)) {
+      _format = _adjustDateTimeFormat(_format);
+    } else if (_type == 'Boolean') {
+      var tmpFormat = _format.replaceAll('//', '\0');
+      var idx = tmpFormat.indexOf('/');
+      if (idx >= 0) {
+        boolFormat = {
+          true: tmpFormat.substring(0, idx).trim().replaceAll('\0', '/'),
+          false: tmpFormat.substring(idx + 1).trim().replaceAll('\0', '/')
+        };
+      }
+    }
+  }
+
+  String outputText(TreeNode node,
+      {bool oneLine = false, bool noHtml = false, bool formatHtml = false}) {
+    var storedText = node.data[name] ?? '';
+    if (storedText.isEmpty) return '';
+    return _formatOutput(storedText,
+        oneLine: oneLine, noHtml: noHtml, formatHtml: formatHtml);
+  }
+
+  String _formatOutput(String storedText,
+      {bool oneLine = false, bool noHtml = false, bool formatHtml = false}) {
+    var localPrefix = _prefix;
+    var localSuffix = _suffix;
+    if (_type == 'Date') {
+      var inputDateFormat = DateFormat('yyyy-MM-dd');
+      var date = inputDateFormat.parse(storedText);
+      var outputDateFormat = DateFormat(_format);
+      storedText = outputDateFormat.format(date);
+    } else if (_type == 'Time') {
+      var inputTimeFormat = DateFormat('HH:mm:ss.S');
+      var time = inputTimeFormat.parse(storedText);
+      var outputTimeFormat = DateFormat(_format);
+      storedText = outputTimeFormat.format(time);
+    } else if (_type == 'DateTime') {
+      var inputFormat = DateFormat('yyyy-MM-dd HH:mm:ss.S');
+      var dateTime = inputFormat.parse(storedText);
+      var outputFormat = DateFormat(_format);
+      storedText = outputFormat.format(dateTime);
+    } else if (_type == 'Boolean') {
+      switch (storedText.toLowerCase()) {
+        case 'true':
+        case 'yes':
+          storedText = boolFormat[true]!;
+          break;
+        case 'false':
+        case 'no':
+          storedText = boolFormat[false]!;
+          break;
+      }
+    }
+    if (oneLine)
+      storedText = RegExp(r'(.+?)<br\s*/?>', caseSensitive: false)
+              .matchAsPrefix(storedText)
+              ?.group(1) ??
+          storedText;
+    if (noHtml) {
+      storedText = removeMarkup(storedText);
+      if (formatHtml) {
+        localPrefix = removeMarkup(localPrefix);
+        localSuffix = removeMarkup(localSuffix);
+      }
+    }
+    if (!formatHtml && !noHtml) {
+      var htmlEscape = HtmlEscape();
+      localPrefix = htmlEscape.convert(localPrefix);
+      localSuffix = htmlEscape.convert(localSuffix);
+    }
+    return '$localPrefix$storedText$localSuffix';
+  }
+}
+
+
+String removeMarkup(String text) {
+  text = text.replaceAll(RegExp(r'<br\s*/?>', caseSensitive: false), '\n');
+  text = text.replaceAll(RegExp(r'<.*?>'), '');
+  return text;
+}
+
+String _adjustDateTimeFormat(String origFormat) {
+  final replacements = const {
+    '%-d': 'd',
+    '%d': 'dd',
+    '%a': 'EEE',
+    '%A': 'EEEE',
+    '%-m': 'M',
+    '%m': 'MM',
+    '%b': 'MMM',
+    '%B': 'MMMM',
+    '%y': 'yy',
+    '%Y': 'yyyy',
+    '%-j': 'D',
+    '%-H': 'H',
+    '%H': 'HH',
+    '%-I': 'h',
+    '%I': 'hh',
+    '%-M': 'm',
+    '%M': 'mm',
+    '%-S': 's',
+    '%S': 'ss',
+    '%f': 'S',
+    '%p': 'a',
+    '%%': "'%'",
+  };
+  final regExp = RegExp(r'%-?[daAmbByYjHIMSfp%]');
+  var newFormat = origFormat.replaceAllMapped(
+      regExp,
+      (Match m) => replacements[m.group(0)] != null
+          ? "'${replacements[m.group(0)]}'"
+          : m.group(0)!);
+  newFormat = "'$newFormat'";
+  newFormat = newFormat.replaceAll("''", "");
+  return newFormat;
+}
